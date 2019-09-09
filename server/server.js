4d8ab4bdfc753bc7771d1cd23e18e883b0dab04a
Irving <imbedoya@tekmexico.com>
2019-09-09 11:16:49 -0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,14 @@ app.listen(3000, () => {
     console.log("Started on port 3000");
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.post('/todos', (req,res) => {
     var todo = new Todo({
         text: req.body.text
